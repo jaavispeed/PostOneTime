ecfcf793bd76df88590520df98e20ba96d9404b2
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-03 19:37:02 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Login from "./components/login/login.jsx"
+import Register from "./components/register/register.jsx"
 
 function App() {
 
   return (
     <div>
-
+      <Login/>
+      
+      {/* <Register/> */}
     </div>
   )
 }
