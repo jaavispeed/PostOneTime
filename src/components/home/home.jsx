7eba43b0a530b8b42c5260989fa7a4a207adb322
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-04 00:29:08 -0300

    home diseño

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,8 @@
 
 import { getCurrentUser, logout } from "../../firebase/Auth.js";
 import { Link } from "react-router";
+import { FiLogOut } from "react-icons/fi";
+
 
 const Home = () => {
 
@@ -19,8 +21,17 @@ const Home = () => {
 
   return (
     <div>
-      <p>{user ? `Usuario: ${user.email}` : "No hay usuario autenticado"}</p>
-      <button className="btn btn-primary" onClick={handleLogout}><Link to="/login">Cerrar sesion</Link></button>
+      <h1 className="text-2xl font-bold text-gray-800">Bienvenido</h1>
+        <p className="text-gray-600 mt-2">
+          {user ? `Usuario: ${user.email}` : "No hay usuario autenticado"}
+        </p>
+      <button
+          onClick={handleLogout}
+          className="mt-4 flex items-center justify-center gap-2 bg-red-500 text-white py-2 px-4 rounded-lg shadow hover:bg-red-600 transition duration-200"
+        >
+          <FiLogOut className="text-xl" /> 
+          <Link to="/login">Cerrar sesión</Link>
+        </button>
     </div>
   )
 }
