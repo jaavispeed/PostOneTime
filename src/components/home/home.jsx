8f14654f0e337f743dc0b00c65bcb4af71d7da47
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-04 00:22:21 -0300

    Sesion cerrada OK

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,27 @@
 
+import { getCurrentUser, logout } from "../../firebase/Auth.js";
+import { Link } from "react-router";
+
 const Home = () => {
+
+  const user = getCurrentUser();
+  console.log(user)
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      console.log("Sesión cerrada");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error.message);
+    }
+  };
+
+
   return (
-    <div>Estas logeado</div>
+    <div>
+      <p>{user ? `Usuario: ${user.email}` : "No hay usuario autenticado"}</p>
+      <button className="btn btn-primary" onClick={handleLogout}><Link to="/login">Cerrar sesion</Link></button>
+    </div>
   )
 }
 
--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,37 +1,45 @@
 
 import appFireBase from "./Credentials";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 const auth = getAuth(appFireBase)
 
 // Función para iniciar sesión
 export const AuthLogin = async (email, password) => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      return userCredential.user;
-    } catch (error) {
-      console.error("Error en login:", error.message);
-      throw error;
-    }
-  };
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    localStorage.setItem("user", JSON.stringify(userCredential.user));
 
-  // Función para registrar un nuevo usuario
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error en login:", error.message);
+    throw error;
+  }
+};
+
+// Función para registrar un nuevo usuario
 export const AuthRegister = async (email, password) => {
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      return userCredential.user;
-    } catch (error) {
-      console.error("Error en registro:", error.message);
-      throw error;
-    }
-  };
-  
-  // Función para cerrar sesión
-  export const logout = async () => {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      console.error("Error al cerrar sesión:", error.message);
-      throw error;
-    }
-  };
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error en registro:", error.message);
+    throw error;
+  }
+};
+
+// Función para cerrar sesión
+export const logout = async () => {
+  try {
+    await signOut(auth);
+    localStorage.removeItem("user");
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error.message);
+    throw error;
+  }
+};
+
+export const getCurrentUser = () => {
+  const localUser = localStorage.getItem("user");
+  return localUser ? JSON.parse(localUser) : null;
+}
